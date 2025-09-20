443ec5c7fbeb4bcf3f4e06b27b1a4b0545effdbe
yash singh bisht <yashbisht0007@gmail.com>
2025-09-21 02:07:15 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import React from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
-import Signup from './pages/signup'
-import Login from './pages/login'
+import Home from './pages/Home.jsx'
 
 const App = () => {
   return (
     <div>
       <Home />
-
-
-
-      {/* <Link to='/'>Home</Link>
-      <Link to='/Login'>Login</Link>
-      <Link to='/Singup'>Signup</Link>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Signup' element={<Signup />} />
-      </Routes> */}
     </div>
   )
 }
